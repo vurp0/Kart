473f3b780ea8206b6525587cf1ede026688b853e
Max Sandholm <max@sandholm.org>
2015-12-08 16:15:15 +0200

    Custom shaders work now (slightly awkwardly, but oh well), there's a demo grayscale shader right now

--- core/assets/shaders/blackwhite.frag
+++ core/assets/shaders/blackwhite.frag
@@ -4,13 +4,11 @@
 
 varying vec4 v_color;
 varying vec2 v_texCoords;
+
 uniform sampler2D u_texture;
-uniform mat4 u_projTrans;
 
 void main() {
-        vec3 color = texture2D(u_texture, v_texCoords).rgb;
+        vec4 color = v_color * texture2D(u_texture, v_texCoords);
         float gray = (color.r + color.g + color.b) / 3.0;
-        vec3 grayscale = vec3(gray);
-
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor = vec4(gray, gray, gray, color.w);
 }
--- core/src/org/sandholm/max/kart/KartGameCameraGroupStrategy.java
+++ core/src/org/sandholm/max/kart/KartGameCameraGroupStrategy.java
@@ -1,27 +1,120 @@
 package org.sandholm.max.kart;
 
+import java.util.Comparator;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g3d.decals.CameraGroupStrategy;
+import com.badlogic.gdx.graphics.g3d.decals.Decal;
+import com.badlogic.gdx.graphics.g3d.decals.DecalMaterial;
+import com.badlogic.gdx.graphics.g3d.decals.GroupStrategy;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.Pool;
 
-/**
- * Created by max on 12/8/15.
- */
 public class KartGameCameraGroupStrategy extends CameraGroupStrategy {
-    ShaderProgram kartGameShader;
+    private static final int GROUP_OPAQUE = 0;
+    private static final int GROUP_BLEND = 1;
 
-    public KartGameCameraGroupStrategy(Camera camera) {
+    Pool<Array<Decal>> arrayPool = new Pool<Array<Decal>>(16) {
+        @Override
+        protected Array<Decal> newObject () {
+            return new Array();
+        }
+    };
+    Array<Array<Decal>> usedArrays = new Array<Array<Decal>>();
+    ObjectMap<DecalMaterial, Array<Decal>> materialGroups = new ObjectMap<DecalMaterial, Array<Decal>>();
+
+    Camera camera;
+    ShaderProgram shader;
+
+    public KartGameCameraGroupStrategy (Camera camera) {
         super(camera);
-        String vertexShader = Gdx.files.internal("shaders/blackwhite.vert").readString();
-        String fragmentShader = Gdx.files.internal("shaders/blackwhite.frag").readString();
-        kartGameShader = new ShaderProgram(vertexShader,fragmentShader);
-        ShaderProgram.pedantic = true;
+        setCamera(camera);
+        createDefaultShader();
+    }
+
+    public KartGameCameraGroupStrategy (Camera camera, Comparator<Decal> sorter) {
+        super(camera, sorter);
+        createDefaultShader();
+
+    }
+
+    public void setCamera (Camera camera) {
+        this.camera = camera;
+    }
+
+    public Camera getCamera () {
+        return camera;
     }
 
     @Override
-    public ShaderProgram getGroupShader(int group) {
-        return kartGameShader;
+    public int decideGroup (Decal decal) {
+        return decal.getMaterial().isOpaque() ? GROUP_OPAQUE : GROUP_BLEND;
     }
 
+    @Override
+    public void afterGroup (int group) {
+        if (group == GROUP_BLEND) {
+            Gdx.gl.glDisable(GL20.GL_BLEND);
+        }
+    }
+
+    @Override
+    public void beforeGroups () {
+        Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+        shader.begin();
+        shader.setUniformMatrix("u_projectionViewMatrix", camera.combined);
+        //shader.setUniformi("u_texture", 0);
+    }
+
+    @Override
+    public void afterGroups () {
+        shader.end();
+        Gdx.gl.glDisable(GL20.GL_DEPTH_TEST);
+    }
+
+    private void createDefaultShader () {
+        /*String vertexShader = "attribute vec4 " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+                + "attribute vec4 " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
+                + "attribute vec2 " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
+                + "uniform mat4 u_projectionViewMatrix;\n" //
+                + "varying vec4 v_color;\n" //
+                + "varying vec2 v_texCoords;\n" //
+                + "\n" //
+                + "void main()\n" //
+                + "{\n" //
+                + "   v_color = " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
+                + "   v_texCoords = " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
+                + "   gl_Position =  u_projectionViewMatrix * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+                + "}\n";*/
+        String vertexShader = Gdx.files.internal("shaders/blackwhite.vert").readString();
+        /*String fragmentShader = "#ifdef GL_ES\n" //
+                + "precision mediump float;\n" //
+                + "#endif\n" //
+                + "varying vec4 v_color;\n" //default
+                + "varying vec2 v_texCoords;\n" //
+                + "uniform sampler2D u_texture;\n" //
+                + "void main()\n"//
+                + "{\n" //
+                + "  gl_FragColor = v_color * texture2D(u_texture, v_texCoords);\n" //
+                + "}";*/
+        String fragmentShader = Gdx.files.internal("shaders/blackwhite.frag").readString();
+
+        shader = new ShaderProgram(vertexShader, fragmentShader);
+        if (shader.isCompiled() == false) throw new IllegalArgumentException("couldn't compile shader: " + shader.getLog());
+    }
+
+    @Override
+    public ShaderProgram getGroupShader (int group) {
+        return shader;
+    }
+
+    @Override
+    public void dispose () {
+        if (shader != null) shader.dispose();
+    }
 }
